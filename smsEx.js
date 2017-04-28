0e9fec18184f7317e88087c762f85294692666d6
Rattapum Puttaraksa <darkness_za@hotmail.co.th>
2017-04-28 18:52:14 +0700

    Delete smsEx.js

--- smsEx.js
+++
@@ -1,5 +0,0 @@
-var sms = require('./controllers/sms.js');
-
-var message = new sms.SmsObj("161.246.6.96", 'guest', 'guest', "+66871363515", "alert from monitor");
-
-setTimeout(function(err,res){message.sendSms("+66871363515", "warning at monitors")}, 1000);
